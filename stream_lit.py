1323e172610a54650c046831b692e5b92dbd7de4
Aishwarya-Sachin <101509315+Aishwarya-Sachin@users.noreply.github.com>
2024-01-13 19:43:20 +0530

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -16,8 +16,8 @@ def preprocess_image(img):
         # Use PIL to open the image from BytesIO object
         img = Image.open(img).convert("RGB")
         img = img.resize((224, 224))
-        img_array = np.array(img, dtype=np.uint8)  # Convert to UINT8
-        img_array = img_array / 255.0
+        img_array = np.array(img, dtype=np.float32)  # Convert to FLOAT32
+        img_array /= 255.0
         img_array = np.expand_dims(img_array, axis=0)
         return img_array
     except Exception as e:
@@ -28,19 +28,22 @@ def make_prediction(img):
     if img:
         preprocessed_img = preprocess_image(img)
         if preprocessed_img is not None:
-            # Perform inference with the TensorFlow Lite model
-            input_tensor_index = interpreter.get_input_details()[0]['index']
-            interpreter.set_tensor(input_tensor_index, preprocessed_img)
-            interpreter.invoke()
-            output_tensor_index = interpreter.get_output_details()[0]['index']
-            prediction = interpreter.get_tensor(output_tensor_index)
+            try:
+                # Perform inference with the TensorFlow Lite model
+                input_tensor_index = interpreter.get_input_details()[0]['index']
+                interpreter.set_tensor(input_tensor_index, preprocessed_img)
+                interpreter.invoke()
+                output_tensor_index = interpreter.get_output_details()[0]['index']
+                prediction = interpreter.get_tensor(output_tensor_index)
 
-            predicted_class_index = np.argmax(prediction)
-            predicted_class = class_labels[predicted_class_index]
-            confidence = prediction[0][predicted_class_index]
+                predicted_class_index = np.argmax(prediction)
+                predicted_class = class_labels[predicted_class_index]
+                confidence = prediction[0][predicted_class_index]
 
-            st.success(f"Prediction: {predicted_class} (Confidence: {confidence:.2%})")
-            st.image(img, caption="Captured Image", use_column_width=True)
+                st.success(f"Prediction: {predicted_class} (Confidence: {confidence:.2%})")
+                st.image(img, caption="Captured Image", use_column_width=True)
+            except Exception as e:
+                st.error(f"Error during inference: {e}")
         else:
             st.error("Failed to process the image.")
     else:
